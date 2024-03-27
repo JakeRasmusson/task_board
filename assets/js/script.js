98416ba36e52a25d04b6d68ae418a99fd4f5f5dc
Jake R <jakerasmusson@gmail.com>
2024-03-27 10:44:57 -0500

    add task button active

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,7 @@ const starterTask = [
 let taskList = JSON.parse(localStorage.getItem("tasks")) || starterTask;
 let cardColumns = document.querySelectorAll('.card-lane');
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+let addTaskForm = document.getElementById('inputForm')
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -72,10 +73,32 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-    console.log('success')
+function handleAddTask(e){
+    e.preventDefault()
+    const title = $('#taskTitle').val()
+    const dueDate = $('#dueDate').val()
+    const taskDesc = $('#taskDesc').val()
+    if (title === '' || dueDate === '' || taskDesc === '') {
+        alert('Please fill out all fields!')
+        return
     }
+    taskList.push(
+        {
+            title: title,
+            text: taskDesc,
+            dueDate: dueDate,
+            id : generateTaskId(),
+            laneID: 'todo-cards'}
+    )
+    addTaskForm.reset()
+    renderTaskList()
+    // console.log(title, dueDate, taskDesc)
 
+        }
+
+
+    
+ 
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
@@ -113,9 +136,11 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     renderTaskList()
-    $('#btn1').on('click', handleAddTask)
-
     $('.card-lane').on('click', handleDeleteTask)
+    $('#inputForm').on('submit', handleAddTask)
+    $('#dueDate').datepicker({
+        format: 'yyyy-mm-dd'
+      });
 
     $('.card-lane').droppable({
         accept: '.draggable',
